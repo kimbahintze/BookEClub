7cc94460c3998b72620f4c6e959b27b4d2d8a106
Kim Lundquist <kimbahintze@gmail.com>
2019-02-01 18:59:10 +0100

    beginning layout

---
+++ BookEClub/ViewControllers/SignUpViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SignUpViewController.swift
+//  BookEClub
+//
+//  Created by Kimba Hintze on 2/1/19.
+//  Copyright © 2019 Kim Lundquist. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var joinLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBAction func joinButton(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
