6d450a02893439d5b217765cd15b4c59893dbe71
Kim Lundquist <kimbahintze@gmail.com>
2019-02-28 21:01:39 +0100

    storyboard and view edits

---
+++ BookEClub/ViewControllers/ProjectDetailCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProjectDetailCollectionViewCell.swift
+//  BookEClub
+//
+//  Created by Kimba Hintze on 2/15/19.
+//  Copyright © 2019 Kim Lundquist. All rights reserved.
+//
+
+import UIKit
+
+class ProjectDetailCollectionViewCell: UICollectionViewCell {
+    
+}
