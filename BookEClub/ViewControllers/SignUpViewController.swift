7b8ab77842f193ce50a53f55efef78ac84bb108d
Kim Lundquist <kimbahintze@gmail.com>
2019-04-03 22:52:46 +0200

    got signupVC completed

--- BookEClub/ViewControllers/SignUpViewController.swift
+++ BookEClub/ViewControllers/SignUpViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseDatabase
 
 class SignUpViewController: UIViewController {
     
-    
+    var ref: DatabaseReference!
 
     // MARK: - Outlets
     @IBOutlet weak var joinLabel: UILabel!
+    @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var joinButton: UIButton!
@@ -23,136 +25,88 @@ class SignUpViewController: UIViewController {
     
     // Mark: - Actions
     @IBAction func joinButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
+    
     
     @IBAction func userAgreementButtonTapped(_ sender: Any) {
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        usernameTextField.delegate = self
+        usernameTextField.addTarget(self, action: #selector(validateForm), for: .editingChanged)
+        emailTextField.delegate = self
+        emailTextField.addTarget(self, action: #selector(validateForm), for: .editingChanged)
+        passwordTextField.delegate = self
+        passwordTextField.isSecureTextEntry = true
+        passwordTextField.addTarget(self, action: #selector(validateForm), for: .editingChanged)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func signup(_ sender: Any) {
+        guard let username = usernameTextField.text, !username.isEmpty else { return }
+        guard let email = emailTextField.text, !email.isEmpty else { return }
+        guard let password = passwordTextField.text, !password.isEmpty else { return }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (dataresult, error) in
+            if let error = error {
+                print("Error creating user: \(error.localizedDescription)")
+                self.duplicateUser()
+                return
+            }
+            
+        Auth.auth().createUser(withEmail: email, password: password) { (dataresult, error) in
+            if let error = error {
+                print("Error creating user: \(error.localizedDescription)")
+                return
+            }
+            guard let uuid = Auth.auth().currentUser?.uid else { return }
+            Database.database().reference().child("users").child(uuid).setValue(["username": username, "email": email, "postID": "false"])
+            let sb = UIStoryboard(name: "Home", bundle: nil)
+            let booksVC = sb.instantiateViewController(withIdentifier: "Books")
+            self.present(booksVC, animated: true, completion: nil)
+        }
     }
-    */
-
+    }
+        @objc func validateForm() {
+        let isFormValid = emailTextField.text?.count ?? 0 > 0 &&
+            passwordTextField.text?.count ?? 0 > 0 &&
+            usernameTextField.text?.count ?? 0 > 0
+        if isFormValid {
+            joinButton.isEnabled = true
+        } else {
+            joinButton.isEnabled = false
+        }
+    }
+    
+    func duplicateUser() {
+        let alertController = UIAlertController(title: "Email already in use", message: nil, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Okay", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
 }
 
-//var ref: DatabaseReference!
-//
-//// MARK: - Outlets
-//@IBOutlet weak var usernameTextField: UITextField!
-//@IBOutlet weak var emailTextField: UITextField!
-//@IBOutlet weak var passwordTextField: UITextField!
-//@IBOutlet weak var signupButton: UIButton!
-//@IBOutlet weak var eulaLabel: UILabel!
-//@IBOutlet weak var eulaButton: UIButton!
-//@IBOutlet weak var memberLabel: UILabel!
-//@IBOutlet weak var loginButton: UIButton!
-//
-//override func viewDidLoad() {
-//    super.viewDidLoad()
-//    usernameTextField.delegate = self
-//    usernameTextField.addTarget(self, action: #selector(validateForm), for: .editingChanged)
-//    emailTextField.delegate = self
-//    emailTextField.addTarget(self, action: #selector(validateForm), for: .editingChanged)
-//    passwordTextField.delegate = self
-//    passwordTextField.isSecureTextEntry = true
-//    passwordTextField.addTarget(self, action: #selector(validateForm), for: .editingChanged)
-//    validateForm()
-//    setFonts()
-//}
-//
-//@IBAction func signup(_ sender: Any) {
-//    guard let username = usernameTextField.text, !username.isEmpty else { return }
-//    guard let email = emailTextField.text, !email.isEmpty else { return }
-//    guard let password = passwordTextField.text, !password.isEmpty else { return }
-//
-//    Auth.auth().createUser(withEmail: email, password: password) { (dataresult, error) in
-//        if let error = error {
-//            print("Error creating user: \(error.localizedDescription)")
-//            self.duplicateUser()
-//            return
-//        }
-//
-//        guard let uuid = Auth.auth().currentUser?.uid else { return }
-//        Database.database().reference().child("users").child(uuid).setValue(["username": username, "email": email, "postID": "false"])
-//        let sb = UIStoryboard(name: "Main", bundle: nil)
-//        let postVC = sb.instantiateViewController(withIdentifier: "Post")
-//        self.present(postVC, animated: true, completion: nil)
-//    }
-//}
-//
-//func duplicateUser() {
-//    let alertController = UIAlertController(title: "Email already in use", message: nil, preferredStyle: .alert)
-//    let okAction = UIAlertAction(title: "Okay", style: .default, handler: nil)
-//    alertController.addAction(okAction)
-//    self.present(alertController, animated: true, completion: nil)
-//}
-//
-//@objc func validateForm() {
-//    let isFormValid = emailTextField.text?.count ?? 0 > 0 &&
-//        passwordTextField.text?.count ?? 0 > 0 &&
-//        usernameTextField.text?.count ?? 0 > 0
-//
-//    if isFormValid {
-//        signupButton.isEnabled = true
-//    } else {
-//        signupButton.isEnabled = false
-//    }
-//}
-//
-//@IBAction func loginButtonTapped(_ sender: Any) {
-//    self.dismiss(animated: true, completion: nil)
-//}
-//
-//func setFonts() {
-//    usernameTextField.font = UIFont(name: MontserratMedium, size: 12)
-//    emailTextField.font = UIFont(name: MontserratMedium, size: 12)
-//    passwordTextField.font = UIFont(name: MontserratMedium, size: 12)
-//    signupButton.titleLabel?.font = UIFont(name: MontserratMedium, size: 15)
-//    signupButton.setTitleColor(UIColor.white, for: .normal)
-//    signupButton.backgroundColor = lighterGreen
-//    signupButton.layer.cornerRadius = 15
-//    eulaLabel.font = UIFont(name: MontserratMedium, size: 12)
-//    eulaLabel.textColor = darkGrayColor
-//    eulaButton.setTitleColor(lighterGreen, for: .normal)
-//    eulaButton.titleLabel?.font = UIFont(name: MontserratMedium, size: 12)
-//    memberLabel.textColor = darkGrayColor
-//    memberLabel.font = UIFont(name: MontserratMedium, size: 11)
-//    loginButton.titleLabel?.font = UIFont(name: MontserratMedium, size: 11)
-//    loginButton.setTitleColor(lighterGreen, for: .normal)
-//}
-//}
-//
-//extension SignUpViewController: UITextFieldDelegate {
-//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-//        switch textField {
-//        case usernameTextField:
-//            usernameTextField.resignFirstResponder()
-//            emailTextField.becomeFirstResponder()
-//            break
-//        case emailTextField:
-//            emailTextField.resignFirstResponder()
-//            passwordTextField.becomeFirstResponder()
-//            break
-//        case passwordTextField:
-//            break
-//        default:
-//            break
-//        }
-//        return true
-//    }
-//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        view.endEditing(true)
-//    }
-//}
+extension SignUpViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        switch textField {
+        case usernameTextField:
+            usernameTextField.resignFirstResponder()
+            emailTextField.becomeFirstResponder()
+            break
+        case emailTextField:
+            emailTextField.resignFirstResponder()
+            passwordTextField.becomeFirstResponder()
+            break
+        case passwordTextField:
+            break
+        default:
+            break
+        }
+        return true
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+}
+
